52877ecf47ba13a22b8269850db9ea14b21a03bb
Rustamov-Zarrrukh <zarruhzak@gmail.com>
2025-03-06 09:50:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// let cityName = prompt("название города");
+
+// let persons = [
+//   { name: "John", age: 25, city: "NewYork" },
+//   { name: "Alice", age: 30, city: "London" },
+//   { name: "Bob", age: 22, city: "Paris" },
+//   { name: "Alex", age: 19, city: "London" },
+//   { name: "Tyler", age: 24, city: "NewYork" },
+//   { name: "Constantine", age: 31, city: "Paris" },
+//   { name: "Cole", age: 21, city: "London" },
+// ];
+
+
+// //1. создать функцию findPersonsByCity которая принимает название города из промпта по которому возвращает массив людей из этого города
+
+// let findPersonsByCity = (city, persons) => {
+
+
+
+// }
+
+// findPersonsByCity(cityName, persons)
+
+// // 2. функция calculateAverageAge вычисляет средний возраст в массиве persons
+// let calculateAverageAge = (arr) => {
+//   // пишем код
+// }
+
+// calculateAverageAge(persons)
+
+
+// // 3. функция addNewPerson добавляет нового пользователя в массив по переданным как на примере
+// let newPerson = prompt("Добавить человека");
+
+// let addNewPerson = (newPers) => {
+//   // пишем код
+//   console.log(persons);
+
+
+// }
+
+// addNewPerson(newPerson)
+
+let cityName = prompt("Введите название города:");
+
+
+let persons = [
+  { name: "John", age: 25, city: "NewYork" },
+  { name: "Alice", age: 30, city: "London" },
+  { name: "Bob", age: 22, city: "Paris" },
+  { name: "Alex", age: 19, city: "London" },
+  { name: "Tyler", age: 24, city: "NewYork" },
+  { name: "Constantine", age: 31, city: "Paris" },
+  { name: "Cole", age: 21, city: "London" },
+];
+
+
+let findPersonsByCity = (city, persons) => {
+  return persons.filter(person => person.city === city);
+
+};
+
+console.log(findPersonsByCity(cityName, persons)); // Вывод в консоль найденных людей
+
+// // 2. функция calculateAverageAge вычисляет средний возраст в массиве persons
+let calculateAverageAge = (persons) => {
+  let totalAge = persons.reduce((sum, person) => sum + person.age, 0);
+  return totalAge / persons.length;
+};
+
+console.log("Средний возраст:", calculateAverageAge(persons));
+
+// 3. функция addNewPerson добавляет нового пользователя в массив по переданным как на примере
+let newPerson = prompt("Добавить человека");
+
+let addNewPerson = (newPers) => {
+  // пишем код
+  console.log(persons);
+
+
+}
+
+addNewPerson(newPerson)
